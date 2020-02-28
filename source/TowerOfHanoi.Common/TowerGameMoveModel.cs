753dce8f04f3b0f979686780bb8b17b3f273f505
Cobus Smit <cobus.smit@chillisoft.co.za>
2020-02-28 16:30:31 +0200

    static creation method for move model

--- source/TowerOfHanoi.Common/TowerGameMoveModel.cs
+++ source/TowerOfHanoi.Common/TowerGameMoveModel.cs
@@ -4,5 +4,14 @@
   {
     public PegNumber From { get; set; }
     public PegNumber To { get; set; }
+
+    public static TowerGameMoveModel CreateMove(PegNumber from, PegNumber to)
+    {
+      return new TowerGameMoveModel
+      {
+        From = from,
+        To = to
+      };
+    }
   }
 }
